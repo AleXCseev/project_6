5f1e5d4cfb53fc76fb87a9978cc8ab8a3b3f5ba8
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-04-09 17:53:17 +0300

    add card

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,44 @@ $(function () {
 	$('[data-fancybox]').fancybox({
 		loop: true,
 	});
+
+	if (!String.prototype.padStart) {
+		String.prototype.padStart = function padStart(targetLength, padString) {
+			targetLength = targetLength >> 0; //truncate if number or convert non-number to 0;
+			padString = String((typeof padString !== 'undefined' ? padString : ' '));
+			if (this.length > targetLength) {
+				return String(this);
+			}
+			else {
+				targetLength = targetLength - this.length;
+				if (targetLength > padString.length) {
+					padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+				}
+				return padString.slice(0, targetLength) + String(this);
+			}
+		};
+	}
+
+	function timer () {
+		function runMultiple(hoursSelector, minutesSelector, secondsSelector) {
+			var d = new Date();
+			var h = String(23 - d.getHours()).padStart(2, "0");
+			var m = String(59 - d.getMinutes()).padStart(2, "0");
+			var s = String(60 - d.getSeconds()).padStart(2, "0");
+			$(hoursSelector).text(h)
+			$(minutesSelector).text(m)
+			$(secondsSelector).text(s)
+		}
+		setInterval(function () {
+			runMultiple(".hours", ".minutes", ".seconds")
+		}, 1000);
+	}
+
+	timer()
+
+	$(".reviews").slick({
+		infinite: true,
+  		slidesToShow: 3,
+  		slidesToScroll: 1
+	});
 })
